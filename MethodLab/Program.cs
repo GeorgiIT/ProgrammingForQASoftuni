db234f04e2722d3d7693a9c5bbeabcccf0746747
Georgi Todorov <georgiit98@gmail.com>
2023-10-13 08:32:12 +0300

    a

---
+++ MethodLab/Program.cs
@@ -0,0 +1,14 @@
+﻿static void RepeatString(int num, int power)
+{
+    int sum = 1;
+    for (int i = 0; i < power; i++)
+    {
+        sum *= num;
+    }
+    Console.WriteLine(sum);
+}
+
+int num = int.Parse(Console.ReadLine());
+int power = int.Parse(Console.ReadLine());
+
+RepeatString(num, power);
---
+++ RepeatingString/Program.cs
@@ -0,0 +1,13 @@
+﻿static void RepeatString(string text, int repeat)
+{
+    string repeatedString = text;
+    for (int i = 0; i < repeat; i++)
+    {
+        repeatedString +=  text;
+    }
+    Console.WriteLine(repeatedString) ;
+}
+
+string text = Console.ReadLine();
+int count = int.Parse(Console.ReadLine());
+RepeatString(text, count);
