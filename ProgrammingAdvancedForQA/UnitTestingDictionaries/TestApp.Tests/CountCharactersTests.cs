5e672b35b8d65c08eb0018ef8bd8d15d6cf6fa96
Georgi Todorov <georgiit98@gmail.com>
2023-11-19 13:51:47 +0200

    cm

---
+++ ProgrammingAdvancedForQA/UnitTestingDictionaries/TestApp.Tests/CountCharactersTests.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+
+namespace TestApp.Tests;
+
+public class CountCharactersTests
+{
+    [Test]
+    public void Test_Count_WithEmptyList_ShouldReturnEmptyString()
+    {
+        // Arrange
+        List<string> input = new();
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    // TODO: finish test
+    [Test]
+    public void Test_Count_WithNoCharacters_ShouldReturnEmptyString()
+    {
+        // Arrange
+        List<string> input = new() { };
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_Count_WithSingleCharacter_ShouldReturnCountString()
+    {
+        List<string> input = new() { "1" };
+
+        // Act
+        string result = CountCharacters.Count(input);
+        string expected = "1 -> 1";
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Count_WithMultipleCharacters_ShouldReturnCountString()
+    {
+        List<string> input = new() { "a", "ab", "abc" };
+
+        // Act
+        string result = CountCharacters.Count(input);
+        string expected = "a -> 3\r\nb -> 2\r\nc -> 1";
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Count_WithSpecialCharacters_ShouldReturnCountString()
+    {
+        List<string> input = new() { "#", "@$", "_+=", "##" };
+
+        // Act
+        string result = CountCharacters.Count(input);
+        string expected = "# -> 3\r\n@ -> 1\r\n$ -> 1\r\n_ -> 1\r\n+ -> 1\r\n= -> 1";
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
---
+++ ProgrammingAdvancedForQA/UnitTestingDictionaries/TestApp.Tests/CountRealNumbersTests.cs
@@ -0,0 +1,69 @@
+using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+
+namespace TestApp.Tests;
+
+public class CountRealNumbersTests
+{
+    // TODO: finish test
+    [Test]
+    public void Test_Count_WithEmptyArray_ShouldReturnEmptyString()
+    {
+        int[] input = { };
+
+        // Act
+        string result = CountRealNumbers.Count(input);
+        
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_Count_WithSingleNumber_ShouldReturnCountString()
+    {
+        int[] input = { 2 };
+
+        // Act
+        string result = CountRealNumbers.Count(input);
+        string expected = "2 -> 1";
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Count_WithMultipleNumbers_ShouldReturnCountString()
+    {
+        int[] input = { 2, 5, 2, 8, 1 };
+
+        // Act
+        string result = CountRealNumbers.Count(input);
+        string expected = "1 -> 1\r\n2 -> 2\r\n5 -> 1\r\n8 -> 1";
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Count_WithNegativeNumbers_ShouldReturnCountString()
+    {
+        int[] input = { 2, 5, 2, -8, 1 };
+
+        // Act
+        string result = CountRealNumbers.Count(input);
+        string expected = "-8 -> 1\r\n1 -> 1\r\n2 -> 2\r\n5 -> 1";
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Count_WithZero_ShouldReturnCountString()
+    {
+        int[] input = { 2, 5, 2, -8, 1, 0 };
+
+        // Act
+        string result = CountRealNumbers.Count(input);
+        string expected = "-8 -> 1\r\n0 -> 1\r\n1 -> 1\r\n2 -> 2\r\n5 -> 1";
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
