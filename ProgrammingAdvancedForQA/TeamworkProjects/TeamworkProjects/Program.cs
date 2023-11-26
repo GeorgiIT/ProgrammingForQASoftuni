850782873cce260da8b1ec0ad8bfa060c043c566
Georgi Todorov <georgiit98@gmail.com>
2023-11-26 16:20:54 +0200

    difficulty with Teamwork Project

--- ProgrammingAdvancedForQA/TeamworkProjects/TeamworkProjects/Program.cs
+++ ProgrammingAdvancedForQA/TeamworkProjects/TeamworkProjects/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int countTeams = int.Parse(Console.ReadLine());
+            Dictionary<string, Team> teams = new Dictionary<string, Team>();
+
+            for (int i = 0; i < countTeams; i++)
+            {
+                string[] teamData = Console.ReadLine().Split("-");
+                string creator = teamData[0];
+                string teamName = teamData[1];
+
+                Team team = new Team(teamName, creator);
+                Console.WriteLine($"Team {teamName} has been created by {creator}!");
+                teams.Add(teamName, team);
+            }
+
+            string command = Console.ReadLine();
+            while (command != "end of assignment") 
+            {
+                command = Console.ReadLine();
+                string memberJoining = command.Split("->")[0];
+                string teamJoining = command.Split("->")[1];
+
+                teams[teamJoining].Members.Add(memberJoining );
+
+            }
+
+           foreach(var team in teams.Where(team => team.Value.Members.Count > 0)
+               .OrderByDescending(team => team.Value.Members.Count)
+               .ThenBy(team => team.Key))
+            {
+
+               Console.WriteLine(team.Key);
+                Console.WriteLine("- " + team.Value.Creator);
+                foreach(string member in team.Value.Members.OrderBy(m => m))
+                {
+                    Console.WriteLine("-- "+ member);
+                }
+            }
+               
+
         }
     }
 }
